23aa94e0e0a46da0e54cec663d685bf041103469
Quinntyne Brown <Quinntyne.Brown@shawmedia.ca>
2016-02-26 20:34:45 -0500

    initial code commit

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace inline_null_check
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run<Lamda>();
+        }
+
+        
+    }
+
+    class HyperLink
+    {
+        public string Href { get; set; }
+
+
+    }
+
+    public class Lamda
+    {
+        [Benchmark]
+        public void Something()
+        {
+            var callToActions = new List<HyperLink>();
+            var defaultLink = callToActions.FirstOrDefault() != null ? callToActions.FirstOrDefault().Href : null;
+        }
+    } 
+}
